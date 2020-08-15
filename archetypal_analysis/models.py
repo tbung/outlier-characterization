3a501c11cdb0fffdcd5739219adff88f2c4771e9
Till Bungert <tillbungert@gmail.com>
2020-08-15 21:19:18 +0200

    Archetypal analysis working with pretrained inn

--- archetypal_analysis/models.py
+++ archetypal_analysis/models.py
@@ -4,6 +4,152 @@ import numpy as np
 import FrEIA.framework as Ff
 import FrEIA.modules as Fm
 
+nch = 1
+couplig_type = "GLOW"
+n_blocks = 24
+internal_width = 256
+clamping = 1.5
+fc_dropout = 0.0
+init_scale = 0.03
+add_image_noise = 0.15
+eps = 1.0
+img_width = 32
+
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(c.nch, 128, 5),
+            # nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 256, 5),
+            # nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(6400, 512),
+            # nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            # nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, c.ncl),
+        )
+
+    def forward(self, x):
+        y = self.conv_layers(x)
+        return self.fc_layers(y.reshape(x.size(0), -1))
+
+
+class Generator(nn.Module):
+    def __init__(self, conditional=False):
+        super(Generator, self).__init__()
+        self.conditional = conditional
+        if conditional:
+            self.img_features = nn.Sequential(
+                # input is Z, going into a convolution
+                nn.ConvTranspose2d(c.nz, c.ngf * 4, 4, 1, 0, bias=False),
+                nn.BatchNorm2d(c.ngf * 4),
+                nn.ReLU(True),
+            )
+
+            self.label_features = nn.Sequential(
+                # input is Z, going into a convolution
+                nn.ConvTranspose2d(c.ncl, c.ngf * 4, 4, 1, 0, bias=False),
+                nn.BatchNorm2d(c.ngf * 4),
+                nn.ReLU(True),
+            )
+        else:
+            self.img_features = nn.Sequential(
+                # input is Z, going into a convolution
+                nn.ConvTranspose2d(c.nz, c.ngf * 8, 4, 1, 0, bias=False),
+                nn.BatchNorm2d(c.ngf * 8),
+                nn.ReLU(True),
+            )
+
+        self.main = nn.Sequential(
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(c.ngf * 8, c.ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(c.ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(c.ngf * 4, c.ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(c.ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            # nn.ConvTranspose2d(c.ngf * 2, c.ngf, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(c.ngf),
+            # nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(c.ngf * 2, c.nch, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input, label):
+        x = self.img_features(input)
+        if self.conditional:
+            y = self.label_features(label)
+            x = torch.cat([x, y], dim=1)
+        return self.main(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, c, conditional=False):
+        super(Discriminator, self).__init__()
+        self.conditional = conditional
+        if conditional:
+            self.img_features = nn.Sequential(
+                # input is (nc) x 64 x 64
+                nn.Conv2d(c.nch, c.ndf, 4, 2, 1, bias=False),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Dropout(0.2)
+            )
+
+            self.label_features = nn.Sequential(
+                # input is (nc) x 64 x 64
+                nn.Conv2d(c.ncl, c.ndf, 4, 2, 1, bias=False),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+        else:
+            self.img_features = nn.Sequential(
+                # input is (nc) x 64 x 64
+                nn.Conv2d(c.nch, c.ndf * 2, 4, 2, 1, bias=False),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Dropout(0.2)
+            )
+
+        self.main = nn.Sequential(
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(c.ndf * 2, c.ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(c.ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.2),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(c.ndf * 4, c.ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(c.ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.2),
+            # state size. (ndf*4) x 8 x 8
+            # nn.Conv2d(c.ndf * 4, c.ndf * 8, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(c.ndf * 8),
+            # nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(c.ndf * 8, 1, 4, 1, 0, bias=False),
+            # nn.Sigmoid()
+        )
+
+    def forward(self, input, label):
+        x = self.img_features(input)
+        if self.conditional:
+            y = self.label_features(label)
+            x = torch.cat([x, y], dim=1)
+        return self.main(x)
+
 
 # define model
 class Encoder(nn.Module):
@@ -108,90 +254,90 @@ class DeepAA(nn.Module):
         return self.decoder(t)
 
 
-def INN(
-    coupling_type='GLOW',
-    nch=1,
-    n_blocks=24,
-    internal_width=512,
-    clamping=1.5,
-    fc_dropout=0.0
-):
-    layer_types = {
-        'NICE': Fm.NICECouplingBlock,
-        'RNVP': Fm.RNVPCouplingBlock,
-        'GLOW': Fm.GLOWCouplingBlock,
-        'GIN': Fm.GINCouplingBlock,
-    }
+# def INN(
+#     coupling_type='GLOW',
+#     nch=1,
+#     n_blocks=24,
+#     internal_width=512,
+#     clamping=1.5,
+#     fc_dropout=0.0
+# ):
+#     layer_types = {
+#         'NICE': Fm.NICECouplingBlock,
+#         'RNVP': Fm.RNVPCouplingBlock,
+#         'GLOW': Fm.GLOWCouplingBlock,
+#         'GIN': Fm.GINCouplingBlock,
+#     }
 
-    # cond_size = 10
-    # cond_node = Ff.ConditionNode(cond_size)
+#     # cond_size = 10
+#     # cond_node = Ff.ConditionNode(cond_size)
 
-    def subnet_constructor(ch_in, ch_out):
-        return Fm.F_fully_connected(ch_in, ch_out, dropout=fc_dropout, internal_size=internal_width)
+#     def subnet_constructor(ch_in, ch_out):
+#         return Fm.F_fully_connected(ch_in, ch_out, dropout=fc_dropout, internal_size=internal_width)
 
-    mod_args = {
-                'subnet_constructor': subnet_constructor,
-    }
+#     mod_args = {
+#                 'subnet_constructor': subnet_constructor,
+#     }
 
-    if coupling_type != 'NICE':
-        mod_args['clamp'] = clamping
+#     if coupling_type != 'NICE':
+#         mod_args['clamp'] = clamping
 
-    nodes = [Ff.InputNode(nch, 28, 28, name='inp')]
+#     nodes = [Ff.InputNode(nch, 28, 28, name='inp')]
 
-    nodes.append(Ff.Node([nodes[-1].out0], Fm.flattening_layer, {}, name='flatten'))
+#     nodes.append(Ff.Node([nodes[-1].out0], Fm.flattening_layer, {}, name='flatten'))
 
-    for i in range(n_blocks):
-        nodes.append(Ff.Node([nodes[-1].out0], Fm.permute_layer, {'seed': i},
-                             name=F'permute_{i}'))
+#     for i in range(n_blocks):
+#         nodes.append(Ff.Node([nodes[-1].out0], Fm.permute_layer, {'seed': i},
+#                              name=F'permute_{i}'))
 
-        nodes.append(Ff.Node(
-            [nodes[-1].out0], layer_types[coupling_type], mod_args, name=F'fc_{i}',
-            # conditions=cond_node
-        ))
+#         nodes.append(Ff.Node(
+#             [nodes[-1].out0], layer_types[coupling_type], mod_args, name=F'fc_{i}',
+#             # conditions=cond_node
+#         ))
 
-    nodes.append(Ff.OutputNode([nodes[-1].out0], name='out'))
-    # nodes.append(cond_node)
+#     nodes.append(Ff.OutputNode([nodes[-1].out0], name='out'))
+#     # nodes.append(cond_node)
 
-    return Ff.ReversibleGraphNet(nodes, verbose=False)
-
-
-def subnet_fc(c_in, c_out):
-    width = 392
-    subnet = nn.Sequential(nn.Linear(c_in, width), nn.ReLU(),
-                           nn.Linear(width, width), nn.ReLU(),
-                           nn.Linear(width,  c_out))
-    for l in subnet:
-        if isinstance(l, nn.Linear):
-            nn.init.xavier_normal_(l.weight)
-    subnet[-1].weight.data.fill_(0.)
-    subnet[-1].bias.data.fill_(0.)
-    return subnet
+#     return Ff.ReversibleGraphNet(nodes, verbose=False)
 
 
-def subnet_conv1(c_in, c_out):
-    width = 16
-    subnet = nn.Sequential(nn.Conv2d(c_in, width, 3, padding=1), nn.ReLU(),
-                           nn.Conv2d(width, width, 3, padding=1), nn.ReLU(),
-                           nn.Conv2d(width, c_out, 3, padding=1))
-    for l in subnet:
-        if isinstance(l, nn.Conv2d):
-            nn.init.xavier_normal_(l.weight)
-    subnet[-1].weight.data.fill_(0.)
-    subnet[-1].bias.data.fill_(0.)
-    return subnet
+# def subnet_fc(c_in, c_out):
+#     width = 392
+#     subnet = nn.Sequential(nn.Linear(c_in, width), nn.ReLU(),
+#                            nn.Linear(width, width), nn.ReLU(),
+#                            nn.Linear(width,  c_out))
+#     for l in subnet:
+#         if isinstance(l, nn.Linear):
+#             nn.init.xavier_normal_(l.weight)
+#     subnet[-1].weight.data.fill_(0.)
+#     subnet[-1].bias.data.fill_(0.)
+#     return subnet
 
 
-def subnet_conv2(c_in, c_out):
-    width = 32
-    subnet = nn.Sequential(nn.Conv2d(c_in, width, 3, padding=1), nn.ReLU(),
-                           nn.Conv2d(width, width, 3, padding=1), nn.ReLU(),
-                           nn.Conv2d(width, c_out, 3, padding=1))
-    for l in subnet:
-        if isinstance(l, nn.Conv2d):
-            nn.init.xavier_normal_(l.weight)
-    subnet[-1].weight.data.fill_(0.)
-    subnet[-1].bias.data.fill_(0.)
-    return subnet
+# def subnet_conv1(c_in, c_out):
+#     width = 16
+#     subnet = nn.Sequential(nn.Conv2d(c_in, width, 3, padding=1), nn.ReLU(),
+#                            nn.Conv2d(width, width, 3, padding=1), nn.ReLU(),
+#                            nn.Conv2d(width, c_out, 3, padding=1))
+#     for l in subnet:
+#         if isinstance(l, nn.Conv2d):
+#             nn.init.xavier_normal_(l.weight)
+#     subnet[-1].weight.data.fill_(0.)
+#     subnet[-1].bias.data.fill_(0.)
+#     return subnet
+
+
+# def subnet_conv2(c_in, c_out):
+#     width = 32
+#     subnet = nn.Sequential(nn.Conv2d(c_in, width, 3, padding=1), nn.ReLU(),
+#                            nn.Conv2d(width, width, 3, padding=1), nn.ReLU(),
+#                            nn.Conv2d(width, c_out, 3, padding=1))
+#     for l in subnet:
+#         if isinstance(l, nn.Conv2d):
+#             nn.init.xavier_normal_(l.weight)
+#     subnet[-1].weight.data.fill_(0.)
+#     subnet[-1].bias.data.fill_(0.)
+#     return subnet
 
 
 def construct_nodes():
@@ -263,40 +409,175 @@ class ConvINN(torch.nn.Module):
         self.load_state_dict(data['inn'])
 
 
+def INN():
+    layer_types = {
+        'NICE': Fm.NICECouplingBlock,
+        'RNVP': Fm.RNVPCouplingBlock,
+        'GLOW': Fm.GLOWCouplingBlock,
+        'GIN': Fm.GINCouplingBlock,
+    }
+
+    ndim_x = 32 * 32
+
+    cond_size = 10
+    cond_nodes = [
+        Ff.ConditionNode(10, 16, 16),
+        Ff.ConditionNode(10, 8, 8),
+        Ff.ConditionNode(cond_size)
+    ]
+
+    def subnet_conv(ch_in, ch_out):
+        return nn.Sequential(nn.Conv2d(ch_in, 256,    3, padding=1), nn.ReLU(),
+                             nn.Conv2d(256,   ch_out, 3, padding=1))
+
+    def subnet_conv_1x1(c_in, c_out):
+        return nn.Sequential(nn.Conv2d(c_in, 256,   1), nn.ReLU(),
+                             nn.Conv2d(256,  c_out, 1))
+
+    def subnet_fc(c_in, c_out):
+        return nn.Sequential(nn.Linear(c_in, 512), nn.ReLU(),
+                             nn.Linear(512,  c_out))
+
+    mod_args = {
+                'subnet_constructor': subnet_conv,
+    }
+
+    if couplig_type != 'NICE':
+        mod_args['clamp'] = clamping
+
+    nodes = [Ff.InputNode(nch, img_width, img_width, name='inp')]
+
+    nodes.append(Ff.Node(nodes[-1], Fm.IRevNetDownsampling, {}))
+
+    for i in range(2):
+        nodes.append(Ff.Node(
+            [nodes[-1].out0], layer_types[couplig_type], mod_args,
+            conditions=cond_nodes[0], name=F'conv_high_res_{i}'
+        ))
+        nodes.append(Ff.Node([nodes[-1].out0], Fm.permute_layer, {'seed': i},
+                             name=F'permute_high_res_{i}'))
+
+    nodes.append(Ff.Node(nodes[-1], Fm.IRevNetDownsampling, {}))
+
+    mod_args = {
+                'subnet_constructor': subnet_conv,
+    }
+
+    if couplig_type != 'NICE':
+        mod_args['clamp'] = clamping
+
+    for i in range(4):
+        if i % 2 == 0:
+            mod_args['subnet_constructor'] = subnet_conv_1x1
+        else:
+            mod_args['subnet_constructor'] = subnet_conv
+
+        nodes.append(Ff.Node(
+            [nodes[-1].out0], layer_types[couplig_type], mod_args,
+            conditions=cond_nodes[1], name=F'conv_low_res_{i}'
+        ))
+        nodes.append(Ff.Node([nodes[-1].out0], Fm.permute_layer, {'seed': i},
+                             name=F'permute_low_res_{i}'))
+
+    nodes.append(Ff.Node(nodes[-1], Fm.Flatten, {}, name='flatten'))
+    split_node = Ff.Node(nodes[-1],
+                         Fm.Split1D,
+                         {'split_size_or_sections':
+                          (ndim_x // 4, 3 * ndim_x // 4), 'dim': 0},
+                         name='split')
+    nodes.append(split_node)
+
+    mod_args = {
+                'subnet_constructor': subnet_fc,
+    }
+
+    if couplig_type != 'NICE':
+        mod_args['clamp'] = clamping
+
+    for k in range(4):
+        nodes.append(Ff.Node(
+            [nodes[-1].out0], layer_types[couplig_type], mod_args,
+            conditions=cond_nodes[2], name=F'fc_{i}'
+        ))
+        nodes.append(Ff.Node([nodes[-1].out0], Fm.permute_layer, {'seed': i},
+                             name=F'permute_{i}'))
+
+    nodes.append(Ff.Node([nodes[-1].out0, split_node.out1],
+                         Fm.Concat1d, {'dim': 0}, name='concat'))
+
+    nodes.append(Ff.OutputNode([nodes[-1].out0], name='out'))
+    nodes.extend(cond_nodes)
+
+    return Ff.ReversibleGraphNet(nodes, verbose=False)
+
+
 class INN_AA(nn.Module):
-    def __init__(self, latent_dim, conv=True):
+    def __init__(self, latent_dim, conv=True, weight_norm_exp=1,
+                 weight_norm_constraint=1, interpolation='linear',
+                 pretrained=None):
         super(INN_AA, self).__init__()
 
+        self.weight_norm_exp = weight_norm_exp
+        self.weight_norm_constraint = weight_norm_constraint
+        self.interpolation = interpolation
+
         self.layers_A = nn.Sequential(
-            nn.Linear(28*28, latent_dim + 1),
+            nn.Linear(32*32, latent_dim + 2),
             # nn.Softmax(dim=1)
         )
         self.layers_B = nn.Sequential(
-            nn.Linear(28*28, latent_dim + 1),
+            nn.Linear(32*32, latent_dim + 2),
             # nn.Softmax(dim=0)
         )
         self.layers_C = nn.Sequential(
-            nn.Linear(latent_dim, 28*28),
+            nn.Linear(latent_dim, 32*32),
             # nn.Softmax(dim=0)
         )
 
-        if conv:
-            self.inn = ConvINN()
-        else:
-            self.inn = INN()
+        self.layers_sideinfo = nn.Sequential(
+            nn.Linear(latent_dim, 200),
+            nn.ReLU(),
+            nn.Linear(200, 10),
+            # nn.Softmax(dim=1)
+        )
 
-    def forward(self, x):
-        t = self.inn(x)
+        # if conv:
+        #     self.inn = ConvINN()
+        # else:
+        self.inn = INN()
+
+        if pretrained is not None:
+            self.inn.load_state_dict(dict(filter(lambda x: 'tmp' not in x[0],
+                                                 torch.load(pretrained).items())))
+
+        for p in self.inn.parameters():
+            p.requires_grad = False
+
+    def forward(self, x, cond=None):
+        t = self.inn(x, cond)
         A = self.layers_A(t)
         exp_A = torch.exp(A - A.max())
-        A = exp_A / torch.sum(exp_A, dim=1, keepdim=True)  # ** (1/2)
+        A = (
+            exp_A / torch.sum(exp_A**self.weight_norm_exp, dim=1, keepdim=True)
+            ** (1/self.weight_norm_exp)
+        )
+        A = self.weight_norm_constraint * A
         B = self.layers_B(t)
         exp_B = torch.exp(B - B.max())
-        B = exp_B / torch.sum(exp_B, dim=1, keepdim=True)  # ** (1/2)
+        B = (
+            exp_B / torch.sum(exp_B**self.weight_norm_exp, dim=1, keepdim=True)
+            ** (1/self.weight_norm_exp)
+        )
+        B = self.weight_norm_constraint * B
 
         return t, A, B.T
 
-    def sample(self, A, z_fixed):
-        A_ = torch.sin(A * np.pi * 2/3)
-        t = self.layers_C(A_ @ z_fixed / np.sin(np.pi * 2/3))
-        return self.inn(t, rev=True)
+    def sample(self, A, z_fixed, cond=None):
+        if self.interpolation == 'linear':
+            t = self.layers_C(A @ z_fixed)
+            sideinfo = self.layers_sideinfo(A @ z_fixed)
+        elif self.interpolation == 'slerp':
+            A_ = torch.sin(A * np.pi * 2/3)
+            t = self.layers_C(A_ @ z_fixed / np.sin(np.pi * 2/3))
+            sideinfo = self.layers_sideinfo(A_ @ z_fixed / np.sin(np.pi * 2/3))
+        return self.inn(t, cond, rev=True), sideinfo
--- archetypal_analysis/train.py
+++ archetypal_analysis/train.py
@@ -1,15 +1,18 @@
 from pathlib import Path
 import torch
+import torch.nn.functional as F
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
+# import matplotlib
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
 import models
 import data
-from utils import config
+from utils import config, logger
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -17,6 +20,7 @@ c = config.Config()
 c.parse_args()
 
 writer = SummaryWriter(comment='_INN')
+log = logger.LossLogger()
 
 writer.add_hparams(c.__dict__, {})
 
@@ -24,13 +28,13 @@ checkpoints_path = Path(writer.get_logdir()) / 'checkpoints'
 checkpoints_path.mkdir(exist_ok=True)
 
 
-def tensor2imgs(t):
-    imgrid = torchvision.utils.make_grid(t)
+def tensor2imgs(t, n=8):
+    imgrid = torchvision.utils.make_grid(t, n)
     return imgrid.data.mul(255).clamp(0, 255).byte().cpu().numpy()
 
 
 def build_z_fixed(latent_dim):
-    z_fixed_t = np.zeros([latent_dim, latent_dim + 1])
+    z_fixed_t = np.zeros([latent_dim, latent_dim + 2])
 
     for k in range(0, latent_dim):
         s = 0.0
@@ -46,7 +50,8 @@ def build_z_fixed(latent_dim):
 
             z_fixed_t[k, j] = (-1.0 / float(latent_dim) - s) / z_fixed_t[k, k]
             z_fixed = np.transpose(z_fixed_t)
-    return torch.tensor(z_fixed, device=device, dtype=torch.float)
+    return torch.tensor(z_fixed, device=device, dtype=torch.float,
+                        requires_grad=True)
 
 
 def plot_z_fixed():
@@ -63,8 +68,42 @@ t_prior = torch.distributions.multivariate_normal.MultivariateNormal(
     scale_tril=torch.eye(c.latent_dim, device=device)
 )
 
-model = models.INN_AA(c.latent_dim).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=c.lr)
+model = models.INN_AA(c.latent_dim, interpolation=c.interpolation,
+                      weight_norm_exp=c.weight_norm_exp,
+                      weight_norm_constraint=c.weight_norm_constraint,
+                      pretrained='../mnist/runs_backup/May13_19-39-26_GLaDOS/checkpoints/generator_in.pt').to(device)
+optimizer = torch.optim.Adam(list(filter(lambda p: p.requires_grad,
+                                         model.parameters())) + [z_fixed], lr=c.lr)
+
+fill = torch.zeros((c.ncl, c.ncl, c.img_width, c.img_width), device=device)
+for i in range(c.ncl):
+    fill[i, i, :, :] = 1
+
+
+def make_cond(labels):
+    cond_tensor = torch.zeros(labels.size(0), c.ncl).cuda()
+    if c.conditional:
+        cond_tensor.scatter_(1, labels.view(-1, 1), 1.)
+    else:
+        cond_tensor[:, 0] = 1
+    return cond_tensor
+
+
+targets = torch.tensor(range(10), device=device).repeat_interleave(c.latent_dim
+                                                                   + 2)
+fixed_cond_z = [
+    fill[:, :, :16, :16][targets],
+    fill[:, :, :8, :8][targets],
+    make_cond(targets)
+]
+
+targets = torch.tensor(range(10), device=device).repeat_interleave(10)
+fixed_cond = [
+    fill[:, :, :16, :16][targets],
+    fill[:, :, :8, :8][targets],
+    make_cond(targets)
+]
+
 
 n_iter = 0
 for epoch in range(c.n_epochs):
@@ -72,75 +111,122 @@ for epoch in range(c.n_epochs):
                                  desc=f'{epoch:04d}'):
         optimizer.zero_grad()
 
+        # z_fixed[-1] = 0 * z_fixed[-1]
+
         samples, targets = samples.to(device), targets.to(device)
+        cond = [
+            fill[:, :, :16, :16][targets],
+            fill[:, :, :8, :8][targets],
+            make_cond(targets)
+        ]
 
         samples += c.add_noise_scale * torch.randn_like(samples, device=device)
 
-        t, A, B = model(samples)
+        t, A, B = model(samples, cond)
 
         zz = torch.sum(t**2, dim=1)
         jac = model.inn.log_jacobian(run_forward=False)
 
-        neg_log_likeli = 0.5 * zz - jac
+        # neg_log_likeli = 0.5 * zz - jac
 
-        nllikelihood = torch.mean(neg_log_likeli)
-        at_loss = torch.mean(torch.norm(B @ A @ z_fixed - z_fixed, dim=1))
-        recon_loss = torch.norm(model.sample(A, z_fixed) - samples)
+        recreated, sideinfo = model.sample(A, z_fixed, cond)
+
+        # nllikelihood = torch.mean(neg_log_likeli)
+        nllikelihood = torch.tensor(0, dtype=torch.float, device=device)
+        sample_latent_mean = A @ z_fixed
+        at_loss = torch.mean(torch.norm(B @ sample_latent_mean - z_fixed, dim=1))
+        proto_loss = torch.sum((z_fixed[-1] - torch.mean(sample_latent_mean,
+                                                         dim=0))**2)
+        recon_loss = torch.norm(recreated - samples)
+        class_loss = F.cross_entropy(sideinfo, targets)
 
         loss = (
             nllikelihood
             + c.at_loss_factor * at_loss
             + c.recon_loss_factor * recon_loss
+            + c.class_loss_factor * class_loss
+            + 10 * c.at_loss_factor * proto_loss
         )
 
         loss.backward()
         optimizer.step()
 
+        log.add_loss('nll', nllikelihood)
+        log.add_loss('at', at_loss)
+        log.add_loss('rec', recon_loss)
+        log.add_loss('class', class_loss)
+        log.add_loss('proto', proto_loss)
+
         writer.add_scalar('Losses/Likelihood', nllikelihood, n_iter)
-        # writer.add_scalar('Losses/Class', class_loss, n_iter)
+        writer.add_scalar('Losses/Class', class_loss, n_iter)
         writer.add_scalar('Losses/Archetype', at_loss, n_iter)
         writer.add_scalar('Losses/Reconstruction', recon_loss, n_iter)
 
         n_iter += 1
 
     with torch.no_grad():
-        recreated = model.sample(torch.eye(c.latent_dim + 1, device=device), z_fixed)
-        writer.add_image('Z_fixed', tensor2imgs(recreated), epoch)
+        recreated, _ = model.sample(
+            torch.eye(c.latent_dim + 2, device=device).repeat(10, 1),
+            z_fixed, fixed_cond_z
+        )
+        writer.add_image('Z_fixed', tensor2imgs(recreated, c.latent_dim + 2), epoch)
         # print(torch.argmax(labels, dim=1))
 
         x, y = next(iter(data.train_loader))
-        x = x.to(device)
-        t, A, B = model(x)
-        recreated = model.sample(A, z_fixed)
+        x, y = x.to(device), y.to(device)
+        cond = [
+            fill[:, :, :16, :16][y],
+            fill[:, :, :8, :8][y],
+            make_cond(y)
+        ]
+        t, A, B = model(x, cond)
+        recreated, sideinfo = model.sample(A, z_fixed, cond)
         writer.add_image('Recreated', tensor2imgs(recreated[:64]), epoch)
+        writer.add_scalar(
+            'Losses/Sample_Class_Accuracy',
+            (F.softmax(sideinfo, dim=1).max(dim=1)[1] == y).float().mean(),
+            epoch
+        )
 
-        sample = model.inn(torch.randn(64, 28*28, device=device), rev=True)
-        writer.add_image('Latent Sample', tensor2imgs(sample), epoch)
+        sample = model.inn(torch.randn(100, 32*32, device=device), fixed_cond, rev=True)
+        writer.add_image('Latent Sample', tensor2imgs(sample, 10), epoch)
 
         if epoch % 10 == 0:
             latent_codes = torch.empty(0, c.latent_dim)
             # inn_codes = torch.empty(0, 28*28)
             labels = torch.empty(0, dtype=torch.long)
             for samples, targets in tqdm(data.train_loader, leave=False):
-                samples = samples.to(device)
-                t, A, B = model(samples)
-                A_ = torch.sin(A * np.pi * 2/3)
-                latent_codes = torch.cat([latent_codes, (A_ @ z_fixed /
-                                                         np.sin(np.pi * 2/3)).cpu()], dim=0)
+                samples, targets = samples.to(device), targets.to(device)
+                cond = [
+                    fill[:, :, :16, :16][targets],
+                    fill[:, :, :8, :8][targets],
+                    make_cond(targets)
+                ]
+                t, A, B = model(samples, cond)
+                if c.interpolation == "linear":
+                    latent_codes = torch.cat([latent_codes, (A @ z_fixed).cpu()], dim=0)
+                elif c.interpolation == "slerp":
+                    A_ = torch.sin(A * np.pi * 2/3)
+                    latent_codes = torch.cat([latent_codes, (A_ @ z_fixed /
+                                                             np.sin(np.pi * 2/3)).cpu()], dim=0)
                 # inn_codes = torch.cat([inn_codes, t.cpu()], dim=0)
-                labels = torch.cat([labels, targets], dim=0)
+                labels = torch.cat([labels, targets.cpu()], dim=0)
 
-            if c.latent_dim > 2:
-                _, _, v = torch.svd(latent_codes)
-                latent_codes = latent_codes @ v
+            try:
+                if c.latent_dim > 2:
+                    _, _, v = torch.svd(latent_codes)
+                    latent_codes = latent_codes @ v
 
-            fig, ax = plt.subplots()
-            img = ax.scatter(latent_codes[:, 0], latent_codes[:, 1], alpha=0.4,
-                             c=labels, cmap='tab10', vmin=0, vmax=9)
-            ax.scatter(z_fixed[:, 0].cpu(), z_fixed[:, 1].cpu(), marker='x', s=100, c='k')
-            ax.set_aspect('equal')
-            fig.colorbar(img)
-            writer.add_figure('Latent Space', fig, epoch)
+                fig, ax = plt.subplots()
+                img = ax.scatter(latent_codes[:, 0], latent_codes[:, 1], alpha=0.4,
+                                 c=labels, cmap='tab10', vmin=0, vmax=9)
+                ax.scatter(z_fixed[:, 0].cpu(), z_fixed[:, 1].cpu(), marker='x', s=100, c='k')
+                ax.set_aspect('equal')
+                fig.colorbar(img)
+                writer.add_figure('Latent Space', fig, epoch)
+                writer.flush()
+            except Exception:
+                writer.flush()
 
             # _, _, v = torch.svd(inn_codes)
             # inn_codes = inn_codes @ v
@@ -151,4 +237,6 @@ for epoch in range(c.n_epochs):
             # ax.set_aspect('equal')
             # writer.add_figure('INN Space', fig, epoch)
 
+    log.flush()
     torch.save(model.state_dict(), checkpoints_path / 'innaa.pt')
+    torch.save(z_fixed, checkpoints_path / 'z_fixed.pt')
