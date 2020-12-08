994f20ce50a6332869a0470fb61c5dd62919967f
Till Bungert <tillbungert@gmail.com>
2020-12-08 16:10:53 +0100

    Add cifar as a data set

--- mnist/data.py
+++ mnist/data.py
@@ -13,7 +13,7 @@ def get_dataset(name, train=True):
                 transform=transforms.Compose(
                     [
                         transforms.Pad(2),
-                        # transforms.RandomAffine(0, (0.5, 0.5)),
+                        # transforms.RandomAffine(0, (0.3, 0.3)),
                         transforms.ToTensor(),
                         transforms.Normalize((0.5,), (0.5,)),
                         transforms.Lambda(lambda x: x.permute(0, 2, 1)),
@@ -88,6 +88,25 @@ def get_dataset(name, train=True):
                         transforms.Normalize((0.5,), (0.5,)),
                         transforms.Lambda(lambda x: x.permute(0, 2, 1)),
                         # transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+                    ]
+                ),
+            ),
+            batch_size=512,
+            shuffle=True,
+            pin_memory=True,
+            num_workers=4,
+            drop_last=True,
+        )
+    elif name == "CIFAR10":
+        return torch.utils.data.DataLoader(
+            datasets.CIFAR10(
+                "~/Data",
+                train=train,
+                download=True,
+                transform=transforms.Compose(
+                    [
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
                     ]
                 ),
             ),
