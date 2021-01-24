53d68e7fb51001f0f3f823124fc7410543556683
Till Bungert <tillbungert@gmail.com>
2021-01-25 00:55:09 +0100

    Add FERG sorted by people

--- mnist/data.py
+++ mnist/data.py
@@ -143,6 +143,29 @@ def get_dataset(name, train=True):
             num_workers=4,
             drop_last=True
         )
+    elif name == "FERG_people":
+        return torch.utils.data.DataLoader(
+            datasets.ImageFolder(
+                "~/Data/FERG_people",
+                transform=transforms.Compose(
+                    [
+                        transforms.Resize(32),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            (0.2090, 0.1497, 0.1129), (0.2601, 0.1899, 0.1538)
+                            # (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+                        )
+                    ]
+                ),
+                is_valid_file=lambda f: f not in
+                ['/home/tillb/Data/FERG_people/bonnie/bonnie_surprise_1389.png']
+            ),
+            batch_size=512,
+            shuffle=True,
+            pin_memory=True,
+            num_workers=4,
+            drop_last=True
+        )
 
 
 def denormalize(name, image):
@@ -153,6 +176,9 @@ def denormalize(name, image):
         mean = torch.tensor((0.4914, 0.4822, 0.4465), device=image.device).reshape(-1, 1, 1)
         std = torch.tensor((0.247, 0.243, 0.261), device=image.device).reshape(-1, 1, 1)
     elif name == "FERG_expressions":
+        mean = torch.tensor((0.2090, 0.1497, 0.1129), device=image.device).reshape(-1, 1, 1)
+        std = torch.tensor((0.2601, 0.1899, 0.1538), device=image.device).reshape(-1, 1, 1)
+    elif name == "FERG_people":
         mean = torch.tensor((0.2090, 0.1497, 0.1129), device=image.device).reshape(-1, 1, 1)
         std = torch.tensor((0.2601, 0.1899, 0.1538), device=image.device).reshape(-1, 1, 1)
 
